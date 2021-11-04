8a997b1bc48777c30af9d7f91ee38f83f7196919
Lenovo <Lenovo@DESKTOP-V17PEFS>
2021-11-04 13:46:50 +0300

    Add project files.

---
+++ methods2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods2
+{
+    class Program
+    {
+        static void DisplayAdd(string[] args)      
+        {
+        }
+    }
+}
+static int Subtract(int)
